f48aa7522be8f2f5c848f5f8d572e660cbed4a1f
Aleksashka301 <aleksashka301@gmail.com>
2025-07-09 19:50:21 +0500

    Update created_intent.py

--- temp/created_intent.py
+++ temp/created_intent.py
@@ -8,9 +8,9 @@ from pathlib import Path
 
 def create_intent(project_id, display_name, training_phrases_parts, message_texts):
     intents_client = dialogflow.IntentsClient()
-
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
+
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
