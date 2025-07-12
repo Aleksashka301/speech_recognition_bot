7cc06f227b4f49c083187a399a48fcae4ebc2848
Aleksashka301 <aleksashka301@gmail.com>
2025-07-12 11:30:40 +0500

    Добавлен логгер

--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,5 @@
+import logging
+import telegram
 import os
 import random
 import vk_api
@@ -5,6 +7,17 @@ import vk_api
 from environs import Env
 from google.cloud import dialogflow
 from vk_api.longpoll import VkEventType, VkLongPoll
+
+
+class TelegramLogsHandler(logging.Handler):
+	def __init__(self, bot, admin_chat_id):
+		super().__init__()
+		self.bot = bot
+		self.admin_chat_id = admin_chat_id
+
+	def emit(self, record):
+		log_entry = self.format(record)
+		self.bot.send_message(chat_id=admin_chat_id, text=log_entry)
 
 
 def welcome(project_id, vk, event, language_code='ru'):
@@ -30,7 +43,17 @@ if __name__ == '__main__':
 	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = env.str('GOOGLE_APPLICATION_CREDENTIALS')
 
 	vk_token = env.str('VK_TOKEN')
+	tg_token = env.str('TG_TOKEN')
+	admin_chat_id = env.int('ADMIN_CHAT_ID')
 	project_id = env.str('DIALOG_FLOW_PROJECT_ID')
+
+	bot = telegram.Bot(token=tg_token)
+
+	log_handler = TelegramLogsHandler(bot, admin_chat_id)
+	log_handler.setLevel(logging.WARNING)
+	formatter = logging.Formatter('[vk_bot] %(asctime)s - %(levelname)s - %(message)s')
+	log_handler.setFormatter(formatter)
+	logging.getLogger().addHandler(log_handler)
 
 	vk_session = vk_api.VkApi(token=vk_token)
 	vk = vk_session.get_api()
@@ -38,5 +61,8 @@ if __name__ == '__main__':
 
 	for event in longpoll.listen():
 		if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-			welcome(project_id, vk, event)
+			try:
+				welcome(project_id, vk, event)
+			except Exception as error:
+				logging.exception('')
 
